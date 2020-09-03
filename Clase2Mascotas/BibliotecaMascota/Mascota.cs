6faed40139d8d650b89d1ee734403a59a2f6ea2d
mateomeli <mateomeli7@gmail.com>
2020-09-02 22:10:53 -0300

    Clase 2

---
+++ Clase2Mascotas/BibliotecaMascota/Mascota.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaMascota
+{
+    public class Mascota
+    {
+        private string nombre;
+        private DateTime fechaNac;
+        private string especie;
+
+        public Mascota(string nombre, DateTime fechaNac, string especie)
+        {
+            this.nombre = nombre;
+            this.fechaNac = fechaNac;
+            this.especie = especie;
+        }
+
+        public string MostrarDatos()
+        {
+            /*
+             * StringBuilder stringBuilder = new StringBuilder();
+             * stringBuilder.AppendFormat("Nombre: {0}\n", this.nombre);
+             * stringBuilder.AppendFormat("Fecha de nacimiento: {0}\n",this.fechaNac.ToString("dd/MM/yyyy"));
+             * stringBuilder.AppendFormat("Especie: {0}\n", this.especie);
+             * return stringBuilder.ToString();
+             */
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Nombre: {this.nombre}");
+            stringBuilder.AppendLine($"Fecha de Nacimiento: {this.fechaNac.ToShortDateString()}");
+            stringBuilder.AppendLine($"Fecha de Nacimiento: {this.fechaNac.ToLongDateString()}");
+            stringBuilder.AppendLine($"Especie: {this.especie}");
+
+            return stringBuilder.ToString();
+        }
+
+        public int CalcularEdad()
+        {
+            return DateTime.Today.AddTicks(-this.fechaNac.Ticks).Year - 1;
+        }
+
+    }
+}
---
+++ Clase2Mascotas/Clase2Mascotas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BibliotecaMascota;
+
+namespace Clase2Mascotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mascota mascota;
+
+            mascota = new Mascota("Mateo", new DateTime(1999, 06, 02), "Humano");
+            Console.WriteLine(mascota.MostrarDatos());
+            Console.WriteLine("La edad es " + mascota.CalcularEdad());
+
+            Console.ReadKey();
+        }
+    }
+}
