d8c791b0e5c6f79060cb45010f4641744a511697
mateomeli <mateomeli7@gmail.com>
2020-09-03 21:19:16 -0300

    Clase 3

---
+++ Sobrecargas/Entidades/Matematica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Matematica
+    {
+        public double Sumar (int a, int b)
+        {
+            return a + b;
+        }
+        public float Sumar (float a, float b)
+        {
+            return a + b;
+        }
+    }
+}
