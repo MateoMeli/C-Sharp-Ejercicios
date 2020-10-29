01abe57efe1f25663140f4e67734b75ca3743e9b
MateoMeli <mateomeli7@gmail.com>
2020-10-29 19:11:53 -0300

    Extension

---
+++ MetodosDeExtension/MetodoExtension/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dato = "Hola como andas?";
+            Console.WriteLine("{0} tiene {1} palabras", dato, dato.ContadorPalabras(' '));
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ MetodosDeExtension/MetodoExtension/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoExtension
+{
+    public static class StringExtension
+    {
+        public static int ContadorPalabras(this string dato, char splitter)
+        {
+            return dato.Split(splitter).Length;
+        }
+        public static int Por10(this int dato)
+        {
+            return dato * 10;
+        }
+    }
+}
