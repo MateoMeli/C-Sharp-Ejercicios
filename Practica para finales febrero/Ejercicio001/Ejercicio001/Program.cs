f7c61f53202d75516cb21627ffc6c69fc118c3f4
mateomeli <mateomeli7@gmail.com>
2021-02-11 18:19:45 -0300

    Enrique muestra

---
+++ Practica para finales febrero/Ejercicio001/Ejercicio001/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio001
+{
+    class Program
+    {
+        //1.	Ingresar 5 números por consola, guardándolos en una variable escalar.
+        //Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+        static void Main(string[] args)
+        {
+            Console.Title = "Enrique Gay";
+            int i;
+            int numero;
+            int acumulador = 0;
+            int promedio;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                string num = Console.ReadLine();
+                int.TryParse(num, out numero);
+                if (numero > max)
+                {
+                    max = numero;
+                }
+                if(numero < min)
+                {
+                    min = numero;
+                }
+                acumulador += numero;
+            }
+            promedio = acumulador / 5;
+            Console.WriteLine("El minimo es {0}, el maximo es {1} y el promedio es {2}", min, max, promedio);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practica para finales febrero/Ejercicio002/Ejercicio002/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Ingrese un numero: ");
+            if(int.TryParse(Console.ReadLine(), out num) && num > 0)
+            {
+                Console.WriteLine("Cuadrado: {0}     Cubo {1}", Math.Pow(num, 2), Math.Pow(num, 3));
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("ERROR TIENE QUE SER MAYOR A CERO");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
